06413d52e389af88d4a807861f328c4a62206916
Austin Smith <trunk9slamchest@gmail.com>
2019-08-27 15:05:53 +0000

    Automatically backed up by Learn

--- lib/meal_choice.rb
+++ lib/meal_choice.rb
@@ -7,5 +7,5 @@ def meal_choice(veg1, veg2, protein = "meat")
 
 end
 
-# meal_choice("broccoli", "macaroni")
-# meal_choice("broccoli", "macaroni", "tofu")
+meal_choice("broccoli", "macaroni")
+meal_choice("broccoli", "macaroni", "tofu")
