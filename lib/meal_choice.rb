67e5c0cf6beccda5a49d402157e66397cc09663e
Austin Smith <trunk9slamchest@gmail.com>
2019-08-27 15:04:46 +0000

    Automatically backed up by Learn

--- lib/meal_choice.rb
+++ lib/meal_choice.rb
@@ -1,5 +1,5 @@
 def meal_choice(veg1, veg2, protein = "meat")
-  # phrase = "What a nutritious meal!"
+  phrase = "What a nutritious meal!"
   order = "A plate of #{protein} with #{veg1} and #{veg2}."
   
   puts phrase
